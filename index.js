679d34217497fe0e22541fd62c7b529ddb1a0105
Daniele Belardi <dnlup.dev@gmail.com>
2022-05-23 13:05:47 +0200

    feat: use function instead of codes to handle errors

--- index.js
+++ index.js
@@ -3,70 +3,19 @@
 const path = require('path');
 const fp = require('fastify-plugin');
 const Sentry = require('@sentry/node');
-const defaultAllowedStatusCodes = require('./allowedStatusCodes');
-const { parseRequest, isAutoSessionTrackingEnabled } = require('./lib/utils');
+const {
+    validateOptions,
+    onErrorFactory: defaultErrorFactory,
+    defaultShouldHandleError,
+} = require('./lib/utils');
 
 const PACKAGE_NAME = require(path.resolve(__dirname, 'package.json')).name;
-
-const defaultErrorFactory = ({ allowedStatusCodes }) =>
-    function errorHandler(error, request, reply) {
-        request.log.error(error);
-        const instance = this;
-        // @fastify/sensible explicit internal errors support
-        if (
-            reply.statusCode === 500 &&
-            error.explicitInternalServerError !== true
-        ) {
-            reply.send(new Error('Something went wrong'));
-        } else {
-            reply.send(error);
-        }
-
-        if (!allowedStatusCodes.includes(reply.statusCode)) {
-            instance.Sentry.withScope((scope) => {
-                scope.addEventProcessor((event) =>
-                    parseRequest(event, request)
-                );
-                const client = instance.Sentry.getCurrentHub().getClient();
-                if (client && isAutoSessionTrackingEnabled(client)) {
-                    const isSessionAggregatesMode =
-                        client._sessionFlusher !== undefined;
-                    if (isSessionAggregatesMode) {
-                        const requestSession = scope.getRequestSession();
-                        if (
-                            requestSession &&
-                            requestSession.status !== undefined
-                        ) {
-                            requestSession.status = 'crashed';
-                        }
-                    }
-                }
-                instance.Sentry.captureException(error);
-            });
-        }
-    };
-
-const validateOptions = function (opts) {
-    if (!Array.isArray(opts.allowedStatusCodes)) {
-        opts.allowedStatusCodes = [opts.allowedStatusCodes];
-    }
-    for (const code of opts.allowedStatusCodes) {
-        if (typeof code !== 'number') {
-            throw new Error(
-                `code in allowedStatusCodes must be a number, received ${typeof code}`
-            );
-        }
-    }
-    if (typeof opts.onErrorFactory !== 'function') {
-        throw new Error('onErrorFactory must be a function');
-    }
-};
 
 module.exports = fp(
     function (
         fastify,
         {
-            allowedStatusCodes = defaultAllowedStatusCodes,
+            shouldHandleError = defaultShouldHandleError,
             onErrorFactory = defaultErrorFactory,
             ...sentryOptions
         },
@@ -80,16 +29,14 @@ module.exports = fp(
 
         try {
             validateOptions({
-                allowedStatusCodes,
+                shouldHandleError,
                 onErrorFactory,
             });
         } catch (error) {
             return next(error);
         }
 
-        const onError = onErrorFactory({
-            allowedStatusCodes,
-        });
+        const onError = onErrorFactory(shouldHandleError);
         if (typeof onError !== 'function') {
             return next(new Error('onError handler must be a function'));
         }
